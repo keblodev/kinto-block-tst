4bdff916ffce666aa3c2cf86b6b90e2ed86cd983
Ronan Amsterdam <romanfromrome.com@gmail.com>
2018-07-27 11:43:36 +0800

    added lollLallaamm222

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || '8000'
  */
 app.get('/lol/:name', (req, res) =>
   res.send({
-    message: `Lolssss111aamisak ${req.params.name}`
+    message: `Lolssss222 ${req.params.name}`
   })
 )
 
